f3764c1eeaf806fc42f8d30fc9e110568e800ad4
Bailey <cbgulsby@crimson.ua.edu>
2019-04-24 17:25:39 -0500

    fixed includes in scheduler

--- Scheduler.c
+++ Scheduler.c
@@ -1,6 +1,6 @@
 // #include "Scheduler.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "Process.h"
 
 
@@ -53,13 +53,3 @@ int main()
     
     return 0;
 }
-
-
-// //globals
-
-// int qs[8][1 << 20];
-// int ts[8] = {0,0,0,0,0,0,0,0};
-// int x = 4096;
-// int mem_alloc[4096];
-// int mem_alloc_ptr = 0;
-// int disk = 8192;
