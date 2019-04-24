86a4038b81f9547a74b77ff1b1ac311d134d226f
Bailey <cbgulsby@crimson.ua.edu>
2019-04-23 21:36:04 -0500

    Started Scheduler

--- Scheduler.c
+++ Scheduler.c
@@ -1,3 +1,21 @@
 #include "Scheduler.h"
 #include "stdio.h"
 #include "stdlib.h"
+
+//globals
+
+int qs[8][1 << 20];
+int ts[8] = {0,0,0,0,0,0,0,0};
+int x = 4096;
+int mem_alloc[4096];
+int mem_alloc_ptr = 0;
+int disk = 8192;
+
+//funtion list
+
+
+int main() 
+{
+    
+    return 0;
+}
