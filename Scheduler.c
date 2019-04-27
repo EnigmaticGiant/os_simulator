8c99d8483f81f5edb8a4309c6b20e7375ea9ca83
Will Hoyt <whoyt48@gmail.com>
2019-04-27 02:32:20 -0500

    Seeing if i created and changed branch correctly
    it is 2:31 i have no mind atm

--- Scheduler.c
+++ Scheduler.c
@@ -39,56 +39,10 @@ void putProcessintoPQ(void) {
     return;
     }
     
-/*
-void Process(void){
-int p[100];
-int a[100];
-int t[100];
-int i = 0;
 
-    while(1){
-        while(!feof) {
-            if(priority == -1 || address == -1 || ms == -1){
-                printf("-1 -1 -1\n");                             //printing -1 -1 -1 to tell everything else to stahp
-                exit(1);
-            }
-            p[i] = priority;
-            a[i] = address;
-            t[i] = ms;
-            i++;
-            scanf("%d %d %d", priority, address, ms);
-            while(1){
-                
-            }
-
-        }
-    }
-
-}
-*/
 void process_handler()
 {
-    while(/*CHECK FOR END CODE OF PROCESSING (-1,-1,-1) */)                             //Infinite loop
-    {
-        while(/* CHECK FOR SWITCH CODE OF READING AND WRITING (-9,-9,-9) */)                         // FIFO LOOP
-        {
-            // CHECK IF THE FIRST THING READ IS (-1,-1,-1) BREAK LOOP;
-            // BLOCK QUEUE CHECK
-                
-            // READY QUEUE CHECK
-            for(int i=0; i<3; i++)      //Priority based for loop
-            {
-                //PROCESS TILL YOU BLOCK IT
-            }
-            // PRINT FIFO CODE FOR SWITCH DISK TO SWITCH FROM READING TO WRITING
-            // Close FIFO
-
-            // CHECK IF EVERYTHING HAS BEEN PROCESSED
-                // IF SO SEND (-1,-1,-1) TO DISK
-            // OPEN FIFO FOR READING
-        }
-        // CHECK THE LAST THING READ FOR END CODE
-    }
+   
 }
 
 
